089e3ba1eef822141fcfe284706ad087acad0273
Omar Houssami <72316065+TshiSelle@users.noreply.github.com>
2024-06-19 20:01:36 +0300

    added error handling for announcer

--- src/commands/utilities/announcer.js
+++ src/commands/utilities/announcer.js
@@ -5,6 +5,16 @@ const {
   ChannelType,
 } = require('discord.js');
 
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('announce')
@@ -63,59 +73,95 @@ module.exports = {
         .addChannelTypes(ChannelType.GuildText)
     ),
   async execute(_, interaction) {
-    if (
-      !interaction.member.permissions.has(
-        PermissionsBitField.Flags.Administrator
-      )
-    ) {
-      return interaction.reply({
-        content: 'You do not have permission to use this command.',
+    try {
+      if (
+        !interaction.member.permissions.has(
+          PermissionsBitField.Flags.Administrator
+        )
+      ) {
+        return interaction.reply({
+          content: 'You do not have permission to use this command.',
+          ephemeral: true,
+        });
+      }
+
+      const title = interaction.options.getString('title');
+      if (!title) {
+        throw new Error('Title is required and was not provided.');
+      }
+
+      const content = interaction.options.getString('content');
+      const url = interaction.options.getString('url');
+      if (url && !isValidHttpUrl(url)) {
+        throw new Error(
+          'Provided URL is invalid. Please enter a valid URL starting with http:// or https://'
+        );
+      }
+
+      const authorIcon = interaction.options.getString('author_icon');
+      if (authorIcon && !isValidHttpUrl(authorIcon)) {
+        throw new Error(
+          'Author icon URL is invalid. Please enter a valid URL starting with http:// or https://'
+        );
+      }
+
+      const thumbnail = interaction.options.getString('thumbnail');
+      if (thumbnail && !isValidHttpUrl(thumbnail)) {
+        throw new Error(
+          'Thumbnail URL is invalid. Please enter a valid URL starting with http:// or https://'
+        );
+      }
+
+      const channel =
+        interaction.options.getChannel('channel') || interaction.channel;
+      if (!channel) {
+        throw new Error('Invalid channel or channel could not be accessed.');
+      }
+
+      const embed = new EmbedBuilder()
+        .setColor(
+          interaction.options.getString('color')
+            ? parseInt(
+                interaction.options.getString('color').replace('#', '0x'),
+                16
+              )
+            : 0x00ffff
+        )
+        .setTitle(title)
+        .setDescription(content || 'No content provided.');
+
+      if (url) {
+        embed.setURL(url);
+      }
+
+      const author = interaction.options.getString('author');
+      if (author) {
+        embed.setAuthor({
+          name: author,
+          iconURL: authorIcon,
+        });
+      }
+
+      if (thumbnail) {
+        embed.setThumbnail(thumbnail);
+      }
+
+      const includeFooter = interaction.options.getBoolean('footer') !== false;
+      if (includeFooter) {
+        embed.setFooter({
+          text: interaction.client.user.username,
+          iconURL: interaction.client.user.avatarURL(),
+        });
+      }
+
+      await channel.send({ embeds: [embed] });
+      interaction.reply({ content: 'Announcement sent!', ephemeral: true });
+    } catch (e) {
+      console.error(`Error in announce command: ${e.message}`, e);
+      interaction.reply({
+        content: `Failed to send announcement: ${e.message}`,
         ephemeral: true,
       });
     }
-
-    const title = interaction.options.getString('title');
-    const content = interaction.options.getString('content');
-    const url = interaction.options.getString('url');
-    const channel =
-      interaction.options.getChannel('channel') || interaction.channel;
-
-    const embed = new EmbedBuilder()
-      .setColor(
-        interaction.options.getString('color')
-          ? parseInt(
-              interaction.options.getString('color').replace('#', '0x'),
-              16
-            )
-          : 0x00ffff
-      )
-      .setTitle(title)
-      .setDescription(content);
-
-    if (interaction.options.getString('author')) {
-      embed.setAuthor({
-        name: interaction.options.getString('author'),
-        iconURL: interaction.options.getString('author_icon'),
-      });
-    }
-    if (interaction.options.getString('url')) {
-      embed.setURL(interaction.options.getString('url'));
-    }
-
-    if (interaction.options.getString('thumbnail')) {
-      embed.setThumbnail(interaction.options.getString('thumbnail'));
-    }
-
-    const includeFooter = interaction.options.getBoolean('footer') !== false;
-    if (includeFooter) {
-      embed.setFooter({
-        text: `${interaction.client.user.username}`,
-        iconURL: interaction.client.user.avatarURL(),
-      });
-    }
-
-    await channel.send({ embeds: [embed] });
-
-    interaction.reply({ content: 'Announcement sent!', ephemeral: true });
   },
 };
